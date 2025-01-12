a3b03119bb6a1c658d22bbf64965394c8df68af4
Ignotas Skrebe <ignotas.skrebe@gmail.com>
2025-01-12 23:20:59 +0000

    ignore not found errors

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # spark-operator-apimachinery
 
 Temp hack for shared deps
+
+```bash
+git tag v1.0.5             
+git push origin v1.0.5
+```
--- pkg/serviceclient/delete.go
+++ pkg/serviceclient/delete.go
@@ -2,10 +2,17 @@ package serviceclient
 
 import (
 	"context"
+	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func (c *sparkClient) DeleteSparkApplication(ctx context.Context, Namespace string, name string) error {
-	return c.crdClientSet.SparkoperatorV1beta2().SparkApplications(Namespace).Delete(ctx, name, metav1.DeleteOptions{})
+	err := c.crdClientSet.SparkoperatorV1beta2().SparkApplications(Namespace).Delete(ctx, name, metav1.DeleteOptions{})
+
+	if err != nil && strings.Contains(err.Error(), "not found") {
+		return nil
+	}
+
+	return err
 }
